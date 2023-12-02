1fe1adee6f1e63bd4eeb6217b4dc3816f4e981cf
norbu <93921032+norbu35@users.noreply.github.com>
2023-12-02 21:20:29 +0800

    day 2 part 1

---
+++ 2023/day2/part1.mjs
@@ -0,0 +1,50 @@
+import { fileReader, lineReader } from "../utils/inputReader.mjs";
+
+const inputData = fileReader("input.txt", "utf-8");
+const readLine = lineReader(inputData, "\n");
+
+const constraint = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+function checkGame(gameData) {
+  if (!gameData) {
+    return 0;
+  }
+
+  const gameId = gameData.split(":")[0].split(" ")[1];
+  const dataBody = gameData.split(":")[1].trim();
+  const sets = dataBody.split(";").map((el) => el.trim());
+  let valid = true;
+
+  for (const set of sets) {
+    for (const numberOfColor of set.split(",")) {
+      const trimmedNumberOfColor = numberOfColor.trim();
+      const color = trimmedNumberOfColor.split(" ")[1];
+      const number = trimmedNumberOfColor.split(" ")[0];
+
+      if (number > constraint[color]) {
+        valid = false;
+        break;
+      }
+    }
+  }
+
+  if (valid) {
+    return parseInt(gameId);
+  } else {
+    return 0;
+  }
+}
+
+let result = 0;
+let line;
+
+while ((line = readLine()) !== undefined) {
+  const value = checkGame(line);
+  result += value;
+}
+
+console.log(result);
